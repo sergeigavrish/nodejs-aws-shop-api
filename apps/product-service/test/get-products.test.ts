18f305ff587d20da36862a1272002a337d5c24a9
Sergei Gavrish <sd.gavrish@gmail.com>
2025-02-23 00:35:08 +0100

    task-03: add getProducts unit test

---
+++ apps/product-service/test/get-products.test.ts
@@ -0,0 +1,62 @@
+import { APIGatewayEvent, Callback, Context } from 'aws-lambda';
+import { Product } from '../src/models/product';
+import { getProducts } from '../src/presentation/get-products';
+import { ProductService } from '../src/service/product-service';
+
+jest.mock('../src/service/product-service');
+
+describe.only('getProducts', () => {
+  let event: APIGatewayEvent;
+  let context: Context;
+  let callback: Callback;
+
+  beforeEach(() => {
+    event = {} as APIGatewayEvent;
+    context = {} as Context;
+    callback = () => {};
+    (ProductService as jest.Mock).mockClear();
+  });
+
+  describe('Status 200', () => {
+    it('Should return all products', async () => {
+      const products: Product[] = [
+        {
+          id: 'f47ac10b-58cc-4372-a567-0e02b2c3d479',
+          title: 'Product 1',
+          description: 'Description for Product 1',
+          price: 10.99,
+          count: 100,
+        },
+        {
+          id: 'c9eb182b-1c3e-4c3b-8c3e-1c3e4c3b8c3e',
+          title: 'Product 2',
+          description: 'Description for Product 2',
+          price: 15.49,
+          count: 200,
+        },
+      ];
+
+      (ProductService.prototype.getProducts as jest.Mock).mockResolvedValueOnce(
+        products
+      );
+
+      const response = await getProducts(event, context, callback);
+
+      expect(response.statusCode).toBe(200);
+      expect(JSON.parse(response.body)).toEqual(products);
+    });
+  });
+
+  describe('Status 500', () => {
+    it('Should return "Internal server error" message', async () => {
+      (ProductService.prototype.getProducts as jest.Mock).mockRejectedValueOnce(
+        new Error()
+      );
+
+      const response = await getProducts(event, context, callback);
+
+      expect(response.statusCode).toBe(500);
+      expect(response.body).toBe('Internal server error');
+    });
+  });
+});
