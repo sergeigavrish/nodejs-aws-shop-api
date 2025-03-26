b29dc4a4a6012e312719704f97ef358daa940d38
Sergei Gavrish <sd.gavrish@gmail.com>
2025-03-26 15:42:48 +0100

    task-07: fix cors error

--- apps/authorization-service/src/presentation/basic-authorizer.ts
+++ apps/authorization-service/src/presentation/basic-authorizer.ts
@@ -11,34 +11,19 @@ export const basicAuthorizer: APIGatewayTokenAuthorizerHandler = async (
   try {
     console.log('basicAuthorizer | ', event);
     const [authType, base64Credentials] = event.authorizationToken.split(' ');
-
     if (authType !== 'Basic' || !base64Credentials) {
-      console.log(
-        'basicAuthorizer | Invalid Token',
-        authType,
-        base64Credentials
-      );
-      return getGatewayAuthorizerResult(
-        'unauthorized',
-        'Deny',
-        event.methodArn
-      );
+      throw new Error('Unauthorized: Invalid Authorization header');
     }
     const credentials = Buffer.from(base64Credentials, 'base64').toString(
       'utf-8'
     );
     const [username, password] = credentials.split(':');
     if (!process.env[username] || password !== process.env[username]) {
-      console.log('basicAuthorizer | Invalid credentials', username, password);
-      return getGatewayAuthorizerResult(
-        'unauthorized',
-        'Deny',
-        event.methodArn
-      );
+      return getGatewayAuthorizerResult(username, 'Deny', event.methodArn);
     }
     return getGatewayAuthorizerResult(username, 'Allow', event.methodArn);
   } catch (error) {
     console.error('basicAuthorizer | ', error);
-    return getGatewayAuthorizerResult('unauthorized', 'Deny', event.methodArn);
+    throw new Error(error?.message);
   }
 };
--- apps/import-service-infrastructure/lib/import-service-infrastructure-stack.ts
+++ apps/import-service-infrastructure/lib/import-service-infrastructure-stack.ts
@@ -1,7 +1,8 @@
-import { Stack, StackProps, RemovalPolicy, Fn } from 'aws-cdk-lib';
+import { Stack, StackProps, RemovalPolicy, Fn, Duration } from 'aws-cdk-lib';
 import {
   AuthorizationType,
   LambdaIntegration,
+  ResponseType,
   RestApi,
   TokenAuthorizer,
 } from 'aws-cdk-lib/aws-apigateway';
@@ -109,6 +110,22 @@ export class ImportServiceInfrastructureStack extends Stack {
         stageName: 'dev',
       },
       restApiName: 'Import Service Rest Api',
+      defaultCorsPreflightOptions: { allowOrigins: allowedOrigins },
+    });
+
+    importServiceRestApi.addGatewayResponse('UnauthorizedResponse', {
+      type: ResponseType.UNAUTHORIZED,
+      responseHeaders: {
+        'Access-Control-Allow-Origin': "'*'",
+      },
+      statusCode: '401',
+    });
+    importServiceRestApi.addGatewayResponse('AccessDeniedResponse', {
+      type: ResponseType.ACCESS_DENIED,
+      responseHeaders: {
+        'Access-Control-Allow-Origin': "'*'",
+      },
+      statusCode: '403',
     });
 
     const authorizationServiceBasicAuthorizerArn = Fn.importValue(
@@ -122,10 +139,10 @@ export class ImportServiceInfrastructureStack extends Stack {
 
     const tokenAuthorizer = new TokenAuthorizer(this, 'TokenAuthorizer', {
       handler: authorizationServiceBasicAuthorizer,
+      resultsCacheTtl: Duration.seconds(0),
     });
 
     const importResource = importServiceRestApi.root.addResource('import');
-    importResource.addCorsPreflight({ allowOrigins: allowedOrigins });
     importResource.addMethod(
       'GET',
       new LambdaIntegration(importProductsFileFunction),
